7368fd211708c7ee8661b4fce6b8cc5d6415cd54
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-31 21:47:27 +0300

    Create second and third VC

---
+++ LikeButtonsPractice/LikeButtonsPractice/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  LikeButtonsPractice
+//
+//  Created by Арсентий Халимовский on 31.03.2023.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // some code here
+    }
+}
---
+++ LikeButtonsPractice/LikeButtonsPractice/ThirdViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ThirdViewController.swift
+//  LikeButtonsPractice
+//
+//  Created by Арсентий Халимовский on 31.03.2023.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // some code here
+    }
+}
