9d96d6881a511341897389c1e79928897aa8ecae
Arsenty Halimovsky <ghost12@yandex.ru>
2023-04-01 16:56:47 +0300

    add outlets for buttons and labels

--- LikeButtonsPractice/LikeButtonsPractice/ViewController.swift
+++ LikeButtonsPractice/LikeButtonsPractice/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var labelView1: UILabel!
+    @IBOutlet weak var labelView2: UILabel!
+    @IBOutlet weak var labelView3: UILabel!
+    
+    
+    @IBOutlet weak var likeButtonView1: UIButton!
+    @IBOutlet weak var likeButtonView2: UIButton!
+    @IBOutlet weak var likeButtonView3: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func setupLikeButton1() {
+        
+    }
+    
+    func setupLikeButton2() {
+        
+    }
+    
+    func setupLikeButton3() {
+        
+    }
+    
+    
+    @IBAction func likeButton1Pressed(_ sender: Any) {
+    }
+    
+    @IBAction func likeButton2Pressed(_ sender: Any) {
+    }
+    
+    @IBAction func likeButton3Pressed(_ sender: Any) {
+    }
+    
 }
 
